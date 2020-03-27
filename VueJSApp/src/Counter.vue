60eac6f09a3c8399d5afa5af1e4aa7d7fc692874
antoine-dc <antoinedeconto@gmail.com>
2020-03-27 18:42:07 +0100

    9/16 : Vue-CLI

---
+++ VueJSApp/src/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="counter">
+    {{count}}
+    <button class="counter__button" @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import store from "./store/NotificationsStore.js";
+export default {
+  //Pour exporter notre objet component
+  data() {
+    return {
+      state: store.state
+    };
+  },
+  computed: {
+    count() {
+      return this.state.count;
+    }
+  },
+  methods: {
+    increment() {
+      store.increment();
+    }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  font-weight: bold;
+}
+</style>
+
+<style scoped src="./assets/css/counter.css">
+</style>
